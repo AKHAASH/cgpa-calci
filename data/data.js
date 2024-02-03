f715dd83f5a4e986a02c5d8b735bd96c23954fe2
AKHAASH <akhaashajayjii@gmail.com>
2024-02-03 17:51:02 +0530

    completed cgpa.js

---
+++ data/data.js
@@ -0,0 +1,77 @@
+export const allSemPapers=[
+  {
+    subject:'Basic Electrical, Electronics and Measurement Engineering',
+    credits:3
+  },
+  {
+    subject:'Communicative English',
+    credits:3
+  },
+  {
+    subject:'Engineering Chemistry',
+    credits:3
+  },
+  {
+    subject:'Engineering Physics',
+    credits:3
+  },
+  {
+    subject:'Mathematics - I',
+    credits:4
+  },
+  {
+    subject:'Problem Solving and Fundamentals of C Programming',
+    credits:3
+  },
+  {
+    subject:'C Programming Laboratory',
+    credits:1.5
+  },
+  {
+    subject:'Physics and Chemistry Laboratory',
+    credits:1.5
+  },
+  {
+    subject:'Technical English',
+    credits:3
+  },
+  {
+    subject:'Mathematics - II',
+    credits:4
+  },
+  {
+    subject:'Physics for Information Science',
+    credits:3
+  },
+  {
+    subject:'Introduction to Artificial Intelligence',
+    credits:3
+  },
+  {
+    subject:'Computational Thinking Using Python',
+    credits:3
+  },
+  {
+    subject:'Engineering Graphics',
+    credits:4
+  },
+  {
+    subject:'Engineering Practice Laboratory',
+    credits:1.5
+  },
+  {
+    subject:'Python Programming Laboratory',
+    credits:1.5
+  },
+  {
+    subject:'Professional Skill - I',
+    credits:1
+  }
+];
+export const gradePoints={
+  'o':10,
+  'a+':9,
+  'a':8,
+  'b+':7,
+  'b':6,
+};
